83d2bdf230e4a5e819304c8c1170210b31995a14
fiki budiono <54945180+fikifx@users.noreply.github.com>
2024-03-25 14:36:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,24 +2,17 @@ const CACHE_NAME = 'bromo-fm-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json',
   '/images/bromo-fm.webp',
-  '/images/icons/icon-72x72.png',
-  '/images/icons/icon-96x96.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-144x144.png',
-  '/images/icons/icon-152x152.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-384x384.png',
-  '/images/icons/icon-512x512.png'
+  '/images/bromo-fm-logo.webp',
+  '/styles.css',
+  '/app.js',
+  '/service-worker.js'
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -29,48 +22,10 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-
-        // Clone the request to not consume it
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response to store in cache
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return fetch(event.request);
       })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  const cacheWhitelist = ['bromo-fm-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
